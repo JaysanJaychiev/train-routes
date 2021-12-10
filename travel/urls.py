00b0306b531b8df5a7c4cf52bd43358acf0ff3fd
JaysanJaychiev <jaysan.jaychiev.11@gmail.com>
2021-12-10 16:08:06 +0600

    add routes

--- travel/urls.py
+++ travel/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from routes.views import find_routes, home
+from routes.views import (
+    RouteListView, find_routes, home, add_route, save_route, RouteDetailView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,5 +10,9 @@ urlpatterns = [
     path('trains/', include(('trains.urls', 'trains'))),
     path('', home, name='home'),
     path('find_routes/', find_routes, name='find_routes'),
+    path('add_route/', add_route, name='add_route'),
+    path('save_route/', save_route, name='save_route'),
+    path('list/', RouteListView.as_view(), name='list'),
+    path('detail/<int:pk>/', RouteDetailView.as_view(), name='detail'),
     ]
  
